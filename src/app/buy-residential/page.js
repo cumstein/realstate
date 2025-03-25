34c9b5cbe6612ed1ced388909b1bc873a047865e
Cumstein <kamyabaghaz@gmail.com>
2025-03-26 02:56:31 +0330

    header added

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -4,8 +4,8 @@ async function BuyResidentials({ searchParams }) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/profile`, {
     cache: "no-store",
   });
-  const data = await res.json();
   res.headers["content-type"] = "text/html";
+  const data = await res.json();
 
   if (data.error) return <h3>مشکلی پیش آمده است</h3>;
 
