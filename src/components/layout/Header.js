0cabcbfb4f592136e20a79e5455006bf6e926fbc
Cumstein <kamyabaghaz@gmail.com>
2025-03-25 21:00:10 +0330

    initial deployment

---
+++ src/components/layout/Header.js
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import styles from "@/layout/Header.module.css";
+
+import { FiLogIn } from "react-icons/fi";
+import { FaUserAlt } from "react-icons/fa";
+
+function Header() {
+  const { data } = useSession();
+
+  return (
+    <header className={styles.header}>
+      <div>
+        <ul>
+          <li>
+            <Link href="/">صفحه ی اصلی</Link>
+          </li>
+          <li>
+            <Link href="/buy-residential"> آگهی ها </Link>
+          </li>
+        </ul>
+      </div>
+      {data ? (
+        <div className={styles.login}>
+          <Link href="/dashboard">
+            <FaUserAlt />
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.login}>
+          <Link href="/signin">
+            <FiLogIn />
+            <span>ورود</span>
+          </Link>
+        </div>
+      )}
+    </header>
+  );
+}
+export default Header;
